efa9b47357373d8fc81d66b950633265a4882b80
Blue <blue@Morio>
2015-11-12 11:08:03 +0100

    mise à jour de fonction.h

--- fonctions.c
+++ fonctions.c
@@ -3,26 +3,28 @@
 
 typedef struct _data
 {
-	string addr;
-	char ** listeFichiers;
+  char addr[17];
+  char listeFichiers[128];
 } *Data;
+
 
 Data initData()
 {
 	Data info = malloc(sizeof(struct _data));
-	info->listeFichier = NULL;
 	return info
 }
 
 void addFileInListFile(char * fileName, Data info)
 {
-	if (info->listeFichier == NULL)
-	{
-		info->listeFichier = (*char)malloc(sizeof(*char));
-		info->listeFichier[0] =	malloc((strlen(fileName)+1)*sizeof(char));
-		info->listeFichier[0] = fileName;
-	}
-
-
+    strcpy(inf->listeFichiers,fileName);
 }
 
+void addAddress(Data info, char * addressRecieved)
+{
+  strcpy(inf->addr,addressRecieved);
+}
+
+void addAddressInTable(char ** table, char * address)
+{
+  
+}
--- fonctions.h
+++ fonctions.h
@@ -7,7 +7,19 @@
 #include <sys/types.h>
 #include <string.h>
 
+
+//structure
 Data infoClient;
 
+
+//serveur
+Data initData();
+
+void addFileInListFile(char * fileName, Data info);
+
+void addAddress(Data info, char * addressRecieved);
+
+// Client
+void addAddressInTable(char ** table, char * address)
 
 #endif
